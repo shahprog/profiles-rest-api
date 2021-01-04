d745f5152bc9fc3267aa07f8c0b41081cef01ebb
Shahriyar <mdshahriyaralam552@gmail.com>
2021-01-05 01:27:21 +0600

    Added other viewset methods

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets
 from profiles_api import serializers
 
 
@@ -45,4 +45,59 @@ class HelloApiView(APIView):
 
     def delete(self, requst, pk=None):
         """Delete an object"""
+        return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+    serializer_class = serializers.HelloSerializer
+    
+    def list(self, request):
+        """Return a hello message"""
+
+        a_viewset = [
+            "Uses action (list, create, retrieve, update, partial_update)",
+            "Automatically maps to URLs using routers",
+            "Provides more functionality with less code"
+        ]
+
+        return Response({
+            'message': "hello",
+            'a_viewset': a_viewset
+        })
+    
+    def create(self, request):
+        """Post method"""
+
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            return Response({'message': name})
+        
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+    
+    def retrieve(self, request, pk=None):
+        """GET method"""
+
+        return Response({'method': 'GET'})
+
+
+    def update(self, request, pk=None):
+        """PUT method"""
+
+        return Response({'method': 'PUT'})
+    
+    def partial_update(self, request, pk=None):
+        """PUT method"""
+
+        return Response({'method': 'PATCH'})
+    
+    def destroy(self, request, pk=None):
+        """PUT method"""
+
         return Response({'method': 'DELETE'})
